e82a6cc098d35f347c00cec400d5433ca70f5978
Pradusha-Timalsina <np03cs4a210012@heraldcollege.edu.np>
2023-12-14 18:19:40 +0545

    Started user and product backend

---
+++ Backend/models/productModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please Enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please Enter product Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter product Price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "please Enter product category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please Enter product Stock"],
+    maxLength: [1, "Stock cannot exceed 1 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAT: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("Product", productSchema);
--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const connection = require("./database");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const product = require("./routes/productRoute");
+// const user = require("./routes/userRoute");
+
 dotenv.config({ path: "Backend/.env" }); //Backend vanne folder bata .env vanne file bolako
 
 const port = process.env.PORT || 8080; //process.env ley .env ma vako PORT ko location lai yaa lyauxa
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 );
+
+//database connection
+connection();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+//routes
+// app.use("/api/v1", user);
+app.use("/api/v1", product);
 
 // unhandled promise rejection
 process.on("unhandledRejection", (err) => {
