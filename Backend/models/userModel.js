5610bffe0b4a15fdce52cd985b801c54c60b94e6
Pradusha-Timalsina <np03cs4a210012@heraldcollege.edu.np>
2023-12-19 16:04:22 +0545

    user login and registration

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -60,4 +60,9 @@ userSchema.methods.getJWTToken = function () {
   });
 };
 
+//Compare Password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
