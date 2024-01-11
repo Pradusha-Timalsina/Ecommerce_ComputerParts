1a13e79cc38a0dd4ae034b6020f0f95732d9f765
Pradusha-Timalsina <np03cs4a210012@heraldcollege.edu.np>
2024-01-11 14:23:03 +0545

    Order Management done

---
+++ Backend/controller/orderController.js
@@ -0,0 +1,119 @@
+const Order = require("../models/orderModel");
+const Product = require("../models/productModel");
+const Errorhandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ErrorHandler = require("../utils/errorhandler");
+
+//Create new Order
+exports.newOrder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  const order = await Order.create({
+    shippingInfo,
+    orderItems,
+    paymentInfo,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    paidAt: Date.now(),
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+//get Single order
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+//get logged in user Orders
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+//get all Orders --> Admin
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+
+  res.status(200).json({
+    success: true,
+    totalAmount,
+    orders,
+  });
+});
+
+//update Order Status --> Admin
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("Your order has been already delivered", 400));
+  }
+
+  order.orderStatus = req.body.status;
+
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+
+    order.orderItems.forEach(async (ord) => {
+      const product = await Product.findById(ord.product);
+      product.stock -= ord.quantity;
+      await product.save({ validateBeforeSave: false });
+    });
+  }
+
+  await order.save({ validateBeforeSave: false });
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//Delete Order --> Admin
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("Order not found with this Id", 404));
+  }
+
+  await order.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
 
 //Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
@@ -40,6 +41,7 @@ app.use(cookieParser());
 //routes
 app.use("/api/v1", user);
 app.use("/api/v1", product);
+app.use("/api/v1", order);
 
 //Middleware for Errors
 app.use(errorMiddleware);
