5aecd8c25dbc179fc9cf5a19d6ba6cd3c83eb322
Pradusha-Timalsina <np03cs4a210012@heraldcollege.edu.np>
2024-01-24 19:29:59 +0545

    redux implementation on login

--- Frontend/ecommerce/src/components/LoginSignup/Login.jsx
+++ Frontend/ecommerce/src/components/LoginSignup/Login.jsx
@@ -1,9 +1,40 @@
-import { Fragment } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./login.css";
 import PasswordInput from "./PasswordInput";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors, login } from "../../actions/userAction";
+// import { useAlert } from "react-alert";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  // const alert = useAlert();
+
+  const { error, loading, isAuthenticated } = useSelector(
+    (state) => state.user
+  );
+
+  const [loginEmail, setLogin] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const loginSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login(loginEmail, loginPassword));
+  };
+
+  useEffect(() => {
+    if (error) {
+      // alert.error(error);
+      dispatch(clearErrors());
+    }
+    if (isAuthenticated) {
+      // alert.success("Login Successfully");
+      navigate("/");
+    }
+  }, [dispatch, error, alert, isAuthenticated, navigate]);
   return (
     <Fragment>
       <div className="login_container">
@@ -14,13 +45,16 @@ const Login = () => {
                   Email verified. You can now login.
                 </p>
               )} */}
-            <form className="form_container">
+            <form className="form_container" onSubmit={loginSubmit}>
               <div>
                 <h1>Login</h1>
                 <input
                   type="email"
                   placeholder="Email"
+                  name="email"
                   required
+                  value={loginEmail}
+                  onChange={(e) => setLogin(e.target.value)}
                   className="input"
                 />
               </div>
@@ -28,7 +62,10 @@ const Login = () => {
                 <PasswordInput
                   type="password"
                   placeholder="Password"
+                  name="password"
                   required
+                  value={loginPassword}
+                  onChange={(e) => setLoginPassword(e.target.value)}
                   className="input"
                 />
               </div>
--- Frontend/ecommerce/src/store.js
+++ Frontend/ecommerce/src/store.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { productReducer } from "./reducers/productReducer";
+import { userReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
   products: productReducer,
+  user: userReducer,
 });
 
 let initialState = {};
